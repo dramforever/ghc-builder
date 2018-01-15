936e0295f281e825040f0fd854a5e695be4b5d7d
dramforever <dramforever@live.com>
2018-01-15 19:26:18 +0800

    Revert "Use fetchgitLocal"
    This reverts commit 42e8a16fdc8d303699eaae73a45a372666c1973e.

--- default.nix
+++ default.nix
@@ -1,24 +1,15 @@
-{ nixpkgs ? import <nixpkgs> {}
+{ version
+, nixpkgs ? import <nixpkgs> {}
 }:
 
 with nixpkgs;
 
-let
-    getFile = path:
-        builtins.replaceStrings ["\n"] [""] (builtins.readFile path);
-
-    revision = getFile ./revision.txt;
-    version = getFile ./version.txt;
-
-in rec {
+rec {
     ghc = (haskell.compiler.ghcHEAD.override (oldAttrs: {
         inherit version;
         selfPkgs = packages;
     })).overrideAttrs (oldAttrs: {
-        src = fetchgit {
-            url = "./ghc";
-            rev = revision;
-        };
+        src = ./ghc;
         makeFlags = [ "-j2" ];
     });
 
