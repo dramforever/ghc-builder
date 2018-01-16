0c2049e647cfea5d0d4d614bb85b900fe08a45c4
dramforever <dramforever@live.com>
2018-01-15 23:44:21 -0600

    Update default.nix

--- default.nix
+++ default.nix
@@ -4,13 +4,22 @@
 
 with nixpkgs;
 
+let rev = builtins.replaceStrings ["\n"] [""] (builtins.readFile ./revision.txt); in
+
 rec {
     ghc = (haskell.compiler.ghcHEAD.override (oldAttrs: {
         inherit version;
         selfPkgs = packages;
     })).overrideAttrs (oldAttrs: {
+        inerit rev;
         src = ./ghc;
         makeFlags = [ "-j4" ];
+        preConfigure = ''
+            echo ${version} >VERSION
+            echo ${rev} >GIT_COMMIT_ID
+            ./boot
+            sed -i -e 's|-isysroot /Developer/SDKs/MacOSX10.5.sdk||' configure
+        ''
     });
 
     packages = haskell.packages.ghcHEAD.override (oldAttrs: {
