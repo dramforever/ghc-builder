86140b7aee45ddb701dcbee1126c1136ad042786
dramforever <dramforever@live.com>
2018-07-14 11:30:30 +0800

    update

--- default.nix
+++ default.nix
@@ -9,20 +9,8 @@ in
 rec {
     ghc = (haskell.compiler.ghcHEAD.override (oldAttrs: {
         inherit version;
-        selfPkgs = packages;
     })).overrideAttrs (oldAttrs: {
-        inherit rev;
         src = ./ghc;
         makeFlags = [ "-j4" ];
-        preConfigure = ''
-            echo ${version} >VERSION
-            echo ${rev} >GIT_COMMIT_ID
-            ./boot
-            sed -i -e 's|-isysroot /Developer/SDKs/MacOSX10.5.sdk||' configure
-        '';
-    });
-
-    packages = haskell.packages.ghcHEAD.override (oldAttrs: {
-        inherit ghc;
     });
 }
