f003a3e594c0e7c21d35ea254a0b6044ce156250
dramforever <dramforever@live.com>
2018-08-25 03:15:42 +0000

    Use fixed nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,10 @@
 let
-  pkgs = import <nixpkgs> {
+  nixpkgsSrc = fetchTarball {
+    url = "https://releases.nixos.org/nixpkgs/nixpkgs-18.09pre150352.7c826371c49/nixexprs.tar.xz";
+    sha256 = "1kih50sp79bvzkbs672gdnw8iqlfxy9nmaipcphdjqad7331as1b";
+  };
+
+  pkgs = import nixpkgsSrc {
     overlays = [ (import ./overlay.nix) ];
   };
 
